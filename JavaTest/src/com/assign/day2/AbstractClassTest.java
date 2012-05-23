7a2361d88bdbeb086f6f1570087d84f764c1736f
Srinath R <naath.r@gmail.com>
2012-05-23 19:41:18 +0530

    format changed

--- JavaTest/src/com/assign/day2/AbstractClassTest.java
+++ JavaTest/src/com/assign/day2/AbstractClassTest.java
@@ -1,11 +1,14 @@
 package com.assign.day2;
-abstract class Abstr
-{
-	
+
+abstract class Abstr {
+
 }
+
 public class AbstractClassTest {
 	public static void main(String[] args) {
-		//Abstr a=new Abstr();//We can't create the instance of Abstract class
-	}
+		// Abstr a=new Abstr();//We can't create the instance of Abstract class
 
+		System.out.println("Hi");
+
+	}
 }
