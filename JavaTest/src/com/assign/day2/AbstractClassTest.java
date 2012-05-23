5abb4ce71639bacd701aba6843addf95e9e5b853
Srinath R <naath.r@gmail.com>
2012-05-23 19:52:24 +0530

    sss

--- JavaTest/src/com/assign/day2/AbstractClassTest.java
+++ JavaTest/src/com/assign/day2/AbstractClassTest.java
@@ -8,7 +8,7 @@ public class AbstractClassTest {
 	public static void main(String[] args) {
 		// Abstr a=new Abstr();//We can't create the instance of Abstract class
 
-		System.out.println("Hi");
+		System.out.println("Hi...");
 
 	}
 }
