fffdd73039fb56ba5c496a4b360829b599e432c6
Srinath Rayabarapu <naath.r@gmail.com>
2013-07-04 10:35:48 +0530

    Adding docs and some additional classes

---
+++ JavaTest/src/com/java/core/threads/ReservationCounter.java
@@ -0,0 +1,20 @@
+package com.java.core.threads;
+
+public class ReservationCounter {
+int money;
+synchronized public void bookTicket(int money)
+{
+	this.money=money;
+	Thread t=Thread.currentThread();
+	System.out.println(t.getName() + ":is booking the ticket");
+	try{
+		Thread.sleep(100);
+	  }catch(Exception e)
+	  {}
+   }
+synchronized public void giveChange()
+{
+	Thread t=Thread.currentThread();
+	System.out.println("gives change" +(money-100) +t.getName());
+  }
+}
