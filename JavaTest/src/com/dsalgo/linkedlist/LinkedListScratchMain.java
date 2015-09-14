0f027e96c29635a2d5cbe3abf62cf242c655e86e
srayabar <srayabar@w-srayabar-07.adprod.bmc.com>
2015-09-14 18:45:41 +0530

    Custom Linked list implementation

--- JavaTest/src/com/dsalgo/linkedlist/LinkedListScratchMain.java
+++ JavaTest/src/com/dsalgo/linkedlist/LinkedListScratchMain.java
@@ -3,7 +3,188 @@ package com.dsalgo.linkedlist;
 public class LinkedListScratchMain {
 
 	public static void main(String[] args) {
-		System.out.println("Hi");
+
+		LinkedListCustom custom = new LinkedListCustom();
+
+		custom.add(10);
+		custom.add(20);
+		custom.add(30);		
+		System.out.println(custom.toString());
+		
+		custom.remove(2);
+		System.out.println(custom.toString());
+		
+		custom.add(40,1);
+		System.out.println(custom.toString());
+		
+		System.out.println(custom.get(3));
+		
+	}
+}
+
+/**
+ * custom linked list class where all the impl is there
+ * 
+ * @author srayabar
+ *
+ */
+class LinkedListCustom {
+
+	private Node head;
+	private int listCount;
+
+	public LinkedListCustom() {
+		this.head = new Node(null);
+		this.listCount = 0;
+	}
+
+	/**
+	 * appends data node at the end of the linked list
+	 * 
+	 * @param data
+	 */
+	public void add(Object data) {
+
+		Node temNode = new Node(data);
+		Node currentHead = this.head;
+
+		while (currentHead.getNext() != null) {
+			currentHead = currentHead.getNext();
+		}
+
+		currentHead.setNext(temNode);
+		this.listCount++;
+
+	}
+
+	/**
+	 * inserts a node at specific index
+	 * 
+	 * @param data
+	 * @param next
+	 */
+	public void add(Object data, int index) {
+
+		Node temNode = new Node(data);
+		Node currentHead = this.head;
+
+		for (int i = 1; i < index && currentHead.getNext() != null; i++) {
+			currentHead = currentHead.getNext();
+		}
+
+		temNode.setNext(currentHead.getNext());
+		currentHead.setNext(temNode);
+		this.listCount++;
+
+	}
+
+	public Object get(int index) {
+
+		if (index <= 0) {// 0 or -int case
+			return null;
+		}
+
+		Node currentNode = this.head.getNext();
+		for (int i = 1; i < index; i++) {
+			if (currentNode.getNext() == null) {
+				return null;
+			}
+
+			currentNode = currentNode.getNext(); // more than 2 case
+		}
+		return currentNode.getData(); // default : 1 case
+	}
+
+	public boolean remove(int index) {
+
+		// 0 or -1 case
+		if (index < 1 || index > size()) {
+			return false;
+		}
+
+		Node currentNode = this.head;
+		for (int i = 1; i < index; i++) {// more than 2 case
+			if (currentNode.getNext() == null) {
+				return false;
+			}
+
+			currentNode = currentNode.getNext();
+		}
+
+		currentNode.setNext(currentNode.getNext().getNext());
+		this.listCount--;
+
+		return true;
+	}
+
+	public int size() {
+		return this.listCount;
+	}
+
+	@Override
+	public String toString() {
+
+		Node currentNode = this.head.getNext();
+		String outpust = "";
+		while (currentNode != null) {
+			outpust += currentNode.getData().toString() + ", ";
+			currentNode = currentNode.getNext();
+		}
+
+		return outpust;
+	}
+
+}
+
+/**
+ * Node object to point to next Object in the chain if no link is passed then
+ * null by default
+ * 
+ * @author srayabar
+ */
+class Node {
+
+	Node next;
+	Object data;
+
+	public Node(Object data) {
+		this.data = data;
+		this.next = null;
+	}
+
+	public Node(Object data, Node next) {
+		this.data = data;
+		this.next = next;
+	}
+
+	/**
+	 * @return the next
+	 */
+	public Node getNext() {
+		return this.next;
+	}
+
+	/**
+	 * @param next
+	 *            the next to set
+	 */
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+	/**
+	 * @return the data
+	 */
+	public Object getData() {
+		return this.data;
+	}
+
+	/**
+	 * @param data
+	 *            the data to set
+	 */
+	public void setData(Object data) {
+		this.data = data;
 	}
 
 }
