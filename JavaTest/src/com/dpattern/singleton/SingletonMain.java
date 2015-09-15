13a08eba907a819154b14f410a9d687c617a3772
srayabar <srayabar@w-srayabar-07.adprod.bmc.com>
2015-09-15 19:52:25 +0530

    Singleton and other class changes

---
+++ JavaTest/src/com/dpattern/singleton/SingletonMain.java
@@ -0,0 +1,44 @@
+package com.dpattern.singleton;
+
+import javax.management.RuntimeErrorException;
+
+/**
+ * Singleton Design pattern class demo
+ * 
+ * @author srayabar
+ *
+ */
+public class SingletonMain {
+
+	public static void main(String[] args) {
+		
+		Singleton s1 = Singleton.getInstance();
+		Singleton s2 = Singleton.getInstance();
+		
+		System.out.println("s1 : " + s1.hashCode());
+		System.out.println("s2 : " + s2.hashCode());
+	}
+}
+
+class Singleton {
+	
+	//private static Singleton singleton = new Singleton(); // eager approach - not suggested
+	private static Singleton singleton = null;
+	
+	//private constructor methods will only be called by class loader
+	private Singleton(){
+		if(singleton != null){
+			throw new RuntimeErrorException(null, "Can not create a new instance, please call getInstance() method"); // this avoids to create instance thru Reflection
+		}
+		System.out.println("Creating..");
+	}
+	
+	//only global point of access
+	public static Singleton getInstance(){
+		if(singleton == null){
+			singleton = new Singleton(); //lazy approach
+		}
+		return singleton;
+	}
+	
+}
--- JavaTest/src/com/dsalgo/linkedlist/LinkedListScratchMain.java
+++ JavaTest/src/com/dsalgo/linkedlist/LinkedListScratchMain.java
@@ -186,5 +186,42 @@ class Node {
 	public void setData(Object data) {
 		this.data = data;
 	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((data == null) ? 0 : data.hashCode());
+		result = prime * result + ((next == null) ? 0 : next.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Node other = (Node) obj;
+		if (data == null) {
+			if (other.data != null)
+				return false;
+		} else if (!data.equals(other.data))
+			return false;
+		if (next == null) {
+			if (other.next != null)
+				return false;
+		} else if (!next.equals(other.next))
+			return false;
+		return true;
+	}
 
 }
