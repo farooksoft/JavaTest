3eb285567291391e393bbf64ec7f42618a4337e3
srayabar <srayabar@w-srayabar-07.adprod.bmc.com>
2015-11-17 17:50:18 +0530

    after long time

--- JavaTest/src/Test.java
+++ JavaTest/src/Test.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Calendar;
 
 
@@ -49,8 +53,56 @@ public class Test {
 		
 		//System.out.println((int) - (60 * Double.parseDouble(".150")));
 		
+		System.out.println(callSwitchMethod());
 		
 		
+	}
+	
+	
+	private static String callSwitchMethod() {
+		
+		switch ("abc") {
+		case "abc":
+			
+			if(1 == 1)
+				return "abc";
+			
+			break;
+
+		default:
+			System.out.println("default");
+			break;
+		}
+		
+		
+		System.out.println("hey");
+		
+		return null;
+	}
+
+
+	private static void readLinesUsingFileReader() throws IOException
+	{
+	    File file = new File("c:/temp/data.txt");
+	    FileReader fr = new FileReader(file);
+	    BufferedReader br = new BufferedReader(fr);
+	    String line;
+	    int i =0;
+	    
+	    while((line = br.readLine()) != null){
+	    	
+	    	if(i < 4){
+	    		i++;
+	    		continue;
+	    	}
+	        if(line.contains("password")){
+	            System.out.println(line);
+	        }
+	        i++;
+	    }
+	    
+	    br.close();
+	    fr.close();
 	}
 
 }
