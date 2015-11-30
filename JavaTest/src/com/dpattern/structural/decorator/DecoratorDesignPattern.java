1e58d6aa0c180ccbe60231d5e3e381cfbfdb3b91
srayabar <srayabar@w-srayabar-07.adprod.bmc.com>
2015-11-30 10:24:35 +0530

    Desing patterns code checking

--- JavaTest/src/com/dpattern/structural/decorator/DecoratorDesignPattern.java
+++ JavaTest/src/com/dpattern/structural/decorator/DecoratorDesignPattern.java
@@ -4,15 +4,13 @@ public class DecoratorDesignPattern {
 	
 	public static void main(String[] args) {
 		
-		Shape circle = new Circle();
-		
 		Shape redCircle = new RedShapeDecorator(new Circle());
 		
-		circle.draw();
+		Shape redRectangle = new RedShapeDecorator(new Rectangle());
+		
+		//draws circle and adds extra  functionality
 		redCircle.draw();
-		
-		
+		//draws circle and adds extra  functionality
+		redRectangle.draw();
 	}
-	
 }
-
