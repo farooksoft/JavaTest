6fff4b26d4800a6f521cf010d993cff3da2a7dc4
srayabar <srayabar@w-srayabar-07.adprod.bmc.com>
2016-09-14 21:02:23 +0530

    hackerrank code check in

---
+++ JavaTest/src/com/aaa/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.aaa;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		String abc = "sdshdshd+dsds.shdsd";
+		String[] split = abc.split("\\+");
+		System.out.println(split[0]);
+		System.out.println(split[1]);
+		
+	}
+
+}
--- JavaTest/src/com/aaa/TestDemo.java
+++
@@ -1,8 +0,0 @@
-package com.aaa;
-
-public class TestDemo {
-	
-	public static void main(String[] args) {
-		
-	}
-}
